446caf725db697c8d20bcb6bf650da3a7b559c5e
Tanjim Rafi <62213056+TanjimRafi@users.noreply.github.com>
2020-09-06 20:38:01 +0600

    edited

--- run.R
+++ run.R
@@ -1,12 +1,4 @@
-# ---- Loading libraries ----
-library("shiny")
-library("shinydashboard")
-library("tidyverse")
-library("leaflet")
-library("plotly")
-library("DT")
-library("fs")
-library("wbstats")
+library(shiny)
 
 port <- Sys.getenv('PORT')
 
